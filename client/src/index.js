3e7ee1046b3e601cda467ad81fd1f0e00b13b4ff
Shruti-0709 <shrutishreya0709@gmail.com>
2024-11-03 23:10:44 +0530

    Resolved merge conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import {BrowserRouter as Router} from "react-router-dom"
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css"
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
